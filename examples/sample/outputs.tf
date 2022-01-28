7412383494270140d108883a51db8dddc20885b3
tomarv2 <varuntomar2019@gmail.com>
2022-01-28 10:58:19 -0800

    updating output

--- examples/sample/outputs.tf
+++ examples/sample/outputs.tf
@@ -3,6 +3,20 @@ output "vpc_id" {
   value       = module.databricks_workspace.vpc_id
 }
 
+output "vpc_route_table_ids" {
+  description = "list of VPC route tables IDs"
+  value       = module.databricks_workspace.vpc_route_table_ids
+}
+
+output "vpc_security_group_ids" {
+  description = "list of VPC security group IDs"
+  value       = module.databricks_workspace.vpc_security_group_ids
+}
+output "vpc_subnet_ids" {
+  description = "list of subnet ids within VPC"
+  value       = module.databricks_workspace.vpc_subnet_ids
+}
+
 output "iam_role_arn" {
   description = "iam role arn"
   value       = module.databricks_workspace.iam_role_arn
